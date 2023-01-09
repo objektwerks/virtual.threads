ebb3b6321c2bce404a0df6e2f44bd07318d2c2e0
objektwerks <objektwerks@runbox.com>
2023-01-09 08:23:26 -0500

    added file line count task object

--- src/test/scala/objektwerks/FileLineCountTask.scala
+++ src/test/scala/objektwerks/FileLineCountTask.scala
@@ -5,6 +5,10 @@ import java.util.concurrent.Callable
 import scala.io.{Codec, Source}
 import scala.util.Using
 
+object FileLineCountTask:
+  val tasks = List( FileLineCountTask("./data/data.a.csv"), FileLineCountTask("./data/data.b.csv") )
+  val expectedLineCount = 540_959
+
 class FileLineCountTask(file: String) extends Callable[Int]:
   def call(): Int =
     Using(
