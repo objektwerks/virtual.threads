5f057076ce9997d094d74ce393e98990af5b8623
objektwerks <objektwerks@runbox.com>
2023-01-09 08:10:51 -0500

    refactored VirtualThreadsTest

--- src/test/scala/objektwerks/VirtualThreadsTest.scala
+++ src/test/scala/objektwerks/VirtualThreadsTest.scala
@@ -1,15 +1,11 @@
 package objektwerks
 
-import java.time.Instant
-import java.util.concurrent.{Callable, Executors, Future}
-import jdk.incubator.concurrent.StructuredTaskScope
+import java.util.concurrent.Executors
 
 import org.scalatest.funsuite.AnyFunSuite
 
-import scala.annotation.tailrec
-import scala.io.{Codec, Source}
 import scala.jdk.CollectionConverters.*
-import scala.util.{Failure, Success, Try, Using}
+import scala.util.Using
 
 import objektwerks.FileLineCountTask
 
@@ -17,7 +13,7 @@ class VirtualThreadsTest extends AnyFunSuite:
   val tasks = List( FileLineCountTask("./data/data.a.csv"), FileLineCountTask("./data/data.b.csv") )
   val expectedLineCount = 540_959
 
-  test("virtual threads submit") {
+  test("submit") {
     Using( Executors.newVirtualThreadPerTaskExecutor() ) { executor =>
       val aFuture = executor.submit( () => FileLineCountTask("./data/data.a.csv").call() )
       val bFuture = executor.submit( () => FileLineCountTask("./data/data.b.csv").call() )
@@ -25,7 +21,7 @@ class VirtualThreadsTest extends AnyFunSuite:
     }.fold( error => fail(error.getMessage), lines => assert(lines == expectedLineCount) )
   }
 
-  test("virtual threads invoke all") {
+  test("invoke all") {
     Using( Executors.newVirtualThreadPerTaskExecutor() ) { executor =>
       executor.invokeAll( tasks.asJava ).asScala.map( future => future.get() ).sum
     }.fold( error => fail(error.getMessage), lines => assert(lines == expectedLineCount) )
