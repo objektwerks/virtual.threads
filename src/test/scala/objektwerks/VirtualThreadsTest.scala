b3dd337393d2edd5ab3bd48c31ba2b30dd9c4cf0
objektwerks <objektwerks@runbox.com>
2023-01-09 08:06:49 -0500

    refactored VirtualThreadsTest

--- src/test/scala/objektwerks/VirtualThreadsTest.scala
+++ src/test/scala/objektwerks/VirtualThreadsTest.scala
@@ -12,6 +12,7 @@ import scala.jdk.CollectionConverters.*
 import scala.util.{Failure, Success, Try, Using}
 
 import objektwerks.FileLineCountTask
+
 class VirtualThreadsTest extends AnyFunSuite:
   val tasks = List( FileLineCountTask("./data/data.a.csv"), FileLineCountTask("./data/data.b.csv") )
   val expectedLineCount = 540_959
@@ -21,11 +22,11 @@ class VirtualThreadsTest extends AnyFunSuite:
       val aFuture = executor.submit( () => FileLineCountTask("./data/data.a.csv").call() )
       val bFuture = executor.submit( () => FileLineCountTask("./data/data.b.csv").call() )
       aFuture.get() + bFuture.get()
-    }.fold( error => fail(error.getMessage()), lines => assert(lines == expectedLineCount) )
+    }.fold( error => fail(error.getMessage), lines => assert(lines == expectedLineCount) )
   }
 
   test("virtual threads invoke all") {
     Using( Executors.newVirtualThreadPerTaskExecutor() ) { executor =>
       executor.invokeAll( tasks.asJava ).asScala.map( future => future.get() ).sum
-    }.fold( error => fail(error.getMessage()), lines => assert(lines == expectedLineCount) )
+    }.fold( error => fail(error.getMessage), lines => assert(lines == expectedLineCount) )
   }
