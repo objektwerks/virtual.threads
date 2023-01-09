692d2ab03ea846a568a543f901e260e564ccec57
objektwerks <objektwerks@runbox.com>
2023-01-09 08:02:30 -0500

    reformatted FileLineCountTask

--- src/test/scala/objektwerks/FileLineCountTask.scala
+++ src/test/scala/objektwerks/FileLineCountTask.scala
@@ -6,4 +6,9 @@ import scala.io.{Codec, Source}
 import scala.util.Using
 
 class FileLineCountTask(file: String) extends Callable[Int]:
-  def call(): Int = Using( Source.fromFile(file, Codec.UTF8.name) ) { source => source.getLines().length }.get
+  def call(): Int =
+    Using(
+      Source.fromFile(file, Codec.UTF8.name)
+    ) { source =>
+      source.getLines().length
+    }.get
