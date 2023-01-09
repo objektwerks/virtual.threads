497e29aa92169311d1569cbb8d105fda1172ea91
objektwerks <objektwerks@runbox.com>
2023-01-09 15:11:05 -0500

    refactored file line count task

--- src/test/scala/objektwerks/FileLineCountTask.scala
+++ src/test/scala/objektwerks/FileLineCountTask.scala
@@ -9,10 +9,12 @@ object FileLineCountTask:
   val tasks = List( FileLineCountTask("./data/data.a.csv"), FileLineCountTask("./data/data.b.csv") )
   val expectedLineCount = 540_959
 
-final class FileLineCountTask(file: String) extends Callable[Int]:
-  def call(): Int =
+  def count(file: String): Int =
     Using(
       Source.fromFile(file, Codec.UTF8.name)
     ) { source =>
       source.getLines.length
     }.get
+
+final class FileLineCountTask(file: String) extends Callable[Int]:
+  def call(): Int = FileLineCountTask.count(file)
