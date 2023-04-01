93c0501f2361d2812429dd35a2d250fc7f37e304
objektwerks <objektwerks@runbox.com>
2023-04-01 11:13:34 -0400

    added scoped value test

---
+++ src/test/scala/objektwerks/ScopedValueTest.scala
@@ -0,0 +1,18 @@
+package objektwerks
+
+import jdk.incubator.concurrent.ScopedValue
+import java.util.UUID
+
+import org.scalatest.funsuite.AnyFunSuite
+import org.scalatest.matchers.should.Matchers
+
+class ScopedValueTest extends AnyFunSuite with Matchers:
+  test("scoped value") {
+    val license: ScopedValue[String] = ScopedValue.newInstance()
+    val uuid = UUID.randomUUID.toString
+    val count = ScopedValue
+      .where(license, uuid)
+      .call { () => if license.get.nonEmpty then 1 else -1 }
+    assert(count == 1)
+    assert(!license.isBound)
+  }
