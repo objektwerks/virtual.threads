012fcc35c309ab706a800efbb68810f72569a1c4
objektwerks <objektwerks@runbox.com>
2023-09-24 09:05:23 -0400

    reformatted test

--- src/test/scala/objektwerks/VirtualThreadsTest.scala
+++ src/test/scala/objektwerks/VirtualThreadsTest.scala
@@ -10,16 +10,14 @@ import scala.util.Using
 import FileLineCountTask.*
 
 final class VirtualThreadsTest extends AnyFunSuite:
-  test("submit") {
+  test("submit"):
     Using( Executors.newVirtualThreadPerTaskExecutor() ) { executor =>
       val aFuture = executor.submit( () => FileLineCountTask("./data/data.a.csv").call() )
       val bFuture = executor.submit( () => FileLineCountTask("./data/data.b.csv").call() )
       aFuture.get() + bFuture.get()
     }.fold( error => fail(error.getMessage), lines => assert(lines == expectedLineCount) )
-  }
 
-  test("invoke all") {
+  test("invoke all"):
     Using( Executors.newVirtualThreadPerTaskExecutor() ) { executor =>
       executor.invokeAll( tasks.asJava ).asScala.map( future => future.get() ).sum
     }.fold( error => fail(error.getMessage), lines => assert(lines == expectedLineCount) )
-  }
