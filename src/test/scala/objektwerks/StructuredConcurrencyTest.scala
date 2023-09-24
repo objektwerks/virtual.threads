1f259db3edf69d6501c680493240cc2d630698cb
objektwerks <objektwerks@runbox.com>
2023-09-24 09:03:44 -0400

    reformatted test

--- src/test/scala/objektwerks/StructuredConcurrencyTest.scala
+++ src/test/scala/objektwerks/StructuredConcurrencyTest.scala
@@ -10,7 +10,7 @@ import scala.util.{Failure, Success, Using}
 import FileLineCountTask.*
 
 final class StructuredConcurrencyTest extends AnyFunSuite:
-  test("join") {
+  test("join"):
     val lines = Using( StructuredTaskScope.ShutdownOnFailure() ) { scope =>
       val aLines = scope.fork( () => FileLineCountTask("./data/data.a.csv").call() )
       val bLines = scope.fork( () => FileLineCountTask("./data/data.b.csv").call() )
@@ -21,21 +21,18 @@ final class StructuredConcurrencyTest extends AnyFunSuite:
     lines match
       case Success(count) => assert(count == expectedLineCount)
       case Failure(error) => fail(error.getMessage)
-  }
 
-  test("join until") {
+  test("join until"):
     Using( StructuredTaskScope.ShutdownOnFailure() ) { scope =>
       val futures = tasks.map( task => scope.fork( () => task.call() ) )
       scope.joinUntil( Instant.now().plusMillis(3000) )
       scope.throwIfFailed()
       futures.map( future => future.get() ).sum
     }.fold( error => fail(error.getMessage), lines => assert(lines == expectedLineCount) )
-  }
 
-  test("race") {
+  test("race"):
     Using( StructuredTaskScope.ShutdownOnSuccess[Int]() ) { scope =>
       tasks.foreach( task => scope.fork( () => task.call() ) )
       scope.joinUntil( Instant.now().plusMillis(3000) )
       scope.result()
     }.fold( error => fail(error.getMessage), lines => assert(lines == 270_562 || lines == 270_397) )
-  }
