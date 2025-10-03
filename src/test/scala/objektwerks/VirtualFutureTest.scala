4fad8ec043ac135ea56c5b713415dea96991c908
objektwerks <objektwerks@runbox.com>
2025-10-03 17:36:18 -0400

    refactored virtual future test

--- src/test/scala/objektwerks/VirtualFutureTest.scala
+++ src/test/scala/objektwerks/VirtualFutureTest.scala
@@ -8,41 +8,39 @@ import org.scalatest.matchers.should.Matchers
 import scala.concurrent.ExecutionContext
 import scala.concurrent.Future
 
-import FileLineCountTask.*
-
 final class VirtualFutureTest extends AsyncFunSuite with Matchers:
   given ExecutionContext = ExecutionContext.fromExecutor( Executors.newVirtualThreadPerTaskExecutor() )
 
   test("zip"):
-    Future { countLines("./data/data.a.csv") } zip
-    Future { countLines("./data/data.b.csv") } map
-    { (aLines, bLines) => aLines + bLines shouldBe expectedLineCount }
+    Future { FileLineCountTask.countLines("./data/data.a.csv") } zip
+    Future { FileLineCountTask.countLines("./data/data.b.csv") } map
+    { (aLines, bLines) => aLines + bLines shouldBe FileLineCountTask.expectedLineCount }
 
   test("sequence"):
     for
-      aLines <- Future { countLines("./data/data.a.csv") }
-      bLines <- Future { countLines("./data/data.b.csv") }
-    yield aLines + bLines shouldBe expectedLineCount
+      aLines <- Future { FileLineCountTask.countLines("./data/data.a.csv") }
+      bLines <- Future { FileLineCountTask.countLines("./data/data.b.csv") }
+    yield aLines + bLines shouldBe FileLineCountTask.expectedLineCount
 
   test("parallel"):
-    val aFuture = Future { countLines("./data/data.a.csv") }
-    val bFuture = Future { countLines("./data/data.b.csv") }
+    val aFuture = Future { FileLineCountTask.countLines("./data/data.a.csv") }
+    val bFuture = Future { FileLineCountTask.countLines("./data/data.b.csv") }
     for
       aLines <- aFuture
       bLines <- bFuture
-    yield aLines + bLines shouldBe expectedLineCount
+    yield aLines + bLines shouldBe FileLineCountTask.expectedLineCount
 
   test("recover"):
-    Future { countLines("./data/data.a.csv") } zip
-    Future { countLines("./data/wrong.name.csv") } map
-    { (aLines, bLines) => aLines + bLines shouldBe expectedLineCount } recover
+    Future { FileLineCountTask.countLines("./data/data.a.csv") } zip
+    Future { FileLineCountTask.countLines("./data/wrong.name.csv") } map
+    { (aLines, bLines) => aLines + bLines shouldBe FileLineCountTask.expectedLineCount } recover
     { case _ => -1  } map { result => result shouldBe -1 }
 
   test("recover > for"):
     val result = (
       for
-        aLines <- Future { countLines("./data/data.a.csv") }
-        bLines <- Future { countLines("./data/wrong.name.csv") }
-      yield aLines + bLines shouldBe expectedLineCount
+        aLines <- Future { FileLineCountTask.countLines("./data/data.a.csv") }
+        bLines <- Future { FileLineCountTask.countLines("./data/wrong.name.csv") }
+      yield aLines + bLines shouldBe FileLineCountTask.expectedLineCount
     ).recover { case _ => -1 }
     result map { result => result shouldBe -1 }
