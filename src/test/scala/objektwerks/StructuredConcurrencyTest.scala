8545e38439b55e727b5c79525b36014ad7d66482
objektwerks <objektwerks@runbox.com>
2025-10-03 17:01:29 -0400

    removed join until test

--- src/test/scala/objektwerks/StructuredConcurrencyTest.scala
+++ src/test/scala/objektwerks/StructuredConcurrencyTest.scala
@@ -21,14 +21,6 @@ final class StructuredConcurrencyTest extends AnyFunSuite:
       case Success(count) => assert(count == expectedLineCount)
       case Failure(error) => fail(error.getMessage)
 
-  test("join until"):
-    Using( StructuredTaskScope.ShutdownOnFailure() ) { scope =>
-      val futures = tasks.map( task => scope.fork( () => task.call() ) )
-      scope.joinUntil( Instant.now().plusMillis(3000) )
-      scope.throwIfFailed()
-      futures.map( future => future.get() ).sum
-    }.fold( error => fail(error.getMessage), lines => assert(lines == expectedLineCount) )
-
   test("race"):
     Using( StructuredTaskScope.ShutdownOnSuccess[Int]() ) { scope =>
       tasks.foreach( task => scope.fork( () => task.call() ) )
